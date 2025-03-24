7223e23c41238c9693292f3cd07c1ac9daad2898
Anand-Shukla123 <anandshukla7610@gmail.com>
2025-03-24 19:03:06 +0530

    Update Test5Application.java

--- src/main/java/com/test5/Test5Application.java
+++ src/main/java/com/test5/Test5Application.java
@@ -10,6 +10,7 @@ public class Test5Application {
 	int y = 50;
 	int z = 40;
 	int a = 30;
+	float e= 12.3;
 	public static void main(String[] args) {
 		SpringApplication.run(Test5Application.class, args);
 	}
